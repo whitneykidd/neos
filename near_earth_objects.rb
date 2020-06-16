878990db47ea1981664373a84bef580dfae919da
Whitney Kidd <whitneymkidd@gmail.com>
2020-06-16 10:57:03 -0600

    refactor NearEarthObjects class

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -6,32 +6,41 @@ Figaro.application = Figaro::Application.new(environment: 'production', path: Fi
 Figaro.load
 
 class NearEarthObjects
-  def self.find_neos_by_date(date)
+
+  def initialize(date)
     conn = Faraday.new(
-      url: 'https://api.nasa.gov',
-      params: { start_date: date, api_key: ENV['nasa_api_key']}
-    )
+          url: 'https://api.nasa.gov',
+          params: { start_date: date, api_key: ENV['nasa_api_key']})
+
     asteroids_list_data = conn.get('/neo/rest/v1/feed')
+    @parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+  end
 
-    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
-
-    largest_astroid_diameter = parsed_asteroids_data.map do |astroid|
-      astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
+  def largest_asteroid_diameter
+    @parsed_asteroids_data.map do |asteroid|
+      asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
     end.max { |a,b| a<=> b}
+  end
 
-    total_number_of_astroids = parsed_asteroids_data.count
-    formatted_asteroid_data = parsed_asteroids_data.map do |astroid|
-      {
-        name: astroid[:name],
-        diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
-        miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
-      }
-    end
+  def total_number_of_asteroids
+    @parsed_asteroids_data.count
+  end
 
+  def formatted_asteroid_data
+    @parsed_asteroids_data.map do |asteroid|
+        {
+          name: asteroid[:name],
+          diameter: "#{asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
+          miss_distance: "#{asteroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
+        }
+      end
+  end
+
+  def asteroid_details
     {
-      astroid_list: formatted_asteroid_data,
-      biggest_astroid: largest_astroid_diameter,
-      total_number_of_astroids: total_number_of_astroids
+      asteroid_list: formatted_asteroid_data,
+      biggest_asteroid: largest_asteroid_diameter,
+      total_number_of_asteroids: total_number_of_asteroids
     }
   end
 end
